056ac0f9596bf61f0f34785c6fb52ab0c6d56129
Shabbar Ansari <shabbaransari@gmail.com>
2025-05-03 18:44:41 +0530

    mobile menu from the topr

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('menu-btn').addEventListener('click', () => {
     const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.style.transform = 'translateX(0)';  // Opens the menu
+    mobileMenu.style.transform = 'translateY(0)';  // Opens the menu from top
     document.getElementById('menu-overlay').classList.remove('hidden');
 });
 
 document.getElementById('close-menu').addEventListener('click', () => {
     const mobileMenu = document.getElementById('mobile-menu');
-    mobileMenu.style.transform = 'translateX(-157%)';  // Closes the menu
+    mobileMenu.style.transform = 'translateY(-100%)';  // Closes the menu to top
     document.getElementById('menu-overlay').classList.add('hidden');
 });
